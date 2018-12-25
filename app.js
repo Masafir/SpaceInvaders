774a086eb8efba323669129adffeb8727e2806e2
Masafir-Nova <lachamir@outlook.fr>
2018-12-25 15:59:35 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+var app = {
+
+init : function(){
+
+    app.container = document.querySelector('.container');
+    app.enemy = document.querySelector('#enemy');
+    app.player = document.getElementById('joueur');
+
+  /*   app.container.innerHTML += 'GRIGRICONTAINER1'; */
+
+    app.enemy.innerHTML +='THATS THE ENEMIES';
+ /*    app.container.appendChild(app.enemy); */
+    app.player.innerHTML +='HE THATS ME';
+    app.container
+
+    console.log('HelloWorld');
+}
+};
+
+app.init();
